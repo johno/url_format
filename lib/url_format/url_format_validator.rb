5b06f0b17b080cc09cdf76ae26568354580f7e34
John Otander <johnotander@gmail.com>
2013-12-14 12:11:05 -0700

    Nuke extra method.

--- lib/url_format/url_format_validator.rb
+++ lib/url_format/url_format_validator.rb
@@ -1,13 +1,9 @@
 class UrlFormatValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless URI.parse(value).kind_of?(URI::HTTP)
-      url_format_invalid(record, attribute, value)
+      record.errors[attribute] << (options[:message] || "is invalid")
     end
   rescue URI::InvalidURIError
-    url_format_invalid(record, attribute, value)
-  end
-
-  def url_format_invalid(record, attribute, value)
     record.errors[attribute] << (options[:message] || "is invalid")
   end
 end

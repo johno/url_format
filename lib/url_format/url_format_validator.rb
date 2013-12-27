2147a3986fdbcb022ecdd759f0e9d3d5327a502a
johnotander <johnotander@gmail.com>
2013-12-27 11:28:57 -0700

    Add a check for TLD.

--- lib/url_format/url_format_validator.rb
+++ lib/url_format/url_format_validator.rb
@@ -1,6 +1,6 @@
 class UrlFormatValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless URI.parse(value).kind_of?(URI::HTTP)
+    unless URI.parse(value).kind_of?(URI::HTTP) && value =~ /\.\w{2,6}/
       record.errors[attribute] << (options[:message] || "is invalid")
     end
   rescue URI::InvalidURIError

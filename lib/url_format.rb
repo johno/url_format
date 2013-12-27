55e93e119fce94dad25708010f92a85c7bfc9863
johnotander <johnotander@gmail.com>
2013-12-27 11:38:01 -0700

    Add domain method, use regex from url_formatter.

--- lib/url_format.rb
+++ lib/url_format.rb
@@ -3,4 +3,10 @@ require "url_format/url_format_validator"
 require "uri"
 
 module UrlFormat
+
+  def self.get_domain(url)
+    host = URI.parse(url).host.downcase
+    host.start_with?('www.') ? host[4..-1] : host
+  rescue URI::InvalidURIError
+  end
 end
--- lib/url_format/url_format_validator.rb
+++ lib/url_format/url_format_validator.rb
@@ -1,9 +1,15 @@
 class UrlFormatValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless URI.parse(value).kind_of?(URI::HTTP) && value =~ /\.\w{2,6}/
+    unless URI.parse(value).kind_of?(URI::HTTP) && value =~ url_regexp
       record.errors[attribute] << (options[:message] || "is invalid")
     end
   rescue URI::InvalidURIError
     record.errors[attribute] << (options[:message] || "is invalid")
+  end
+
+  # Thanks to Dean Perry and Ryan Bates 
+  # https://github.com/deanperry/url_formatter/blob/master/lib/url_formatter.rb
+  def url_regexp
+    /\Ahttps?:\/\/([\A\s:@]+:[\A\s:@]*@)?[-[[:alnum:]]]+(\.[-[[:alnum:]]]+)+\.?(:\d{1,5})?([\/?]\S*)?\z/iux
   end
 end
